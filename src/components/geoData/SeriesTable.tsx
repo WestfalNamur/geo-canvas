1f7f075ab8be038801406b8edff0fbcb19809dea
Stefan Crummenerl <crummenerls@gmail.com>
2020-06-08 16:32:16 +0200

    Revert "[DEV] move towards react-tables"
    This reverts commit 19b1be88dacfef6afd3f7e898934c56787fa23a2.

--- src/components/geoData/SeriesTable.tsx
+++ src/components/geoData/SeriesTable.tsx
@@ -1,106 +1,51 @@
-import React, { useMemo } from "react";
+import React from "react";
+import MaterialTable, { Column } from "material-table";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
-import styled from "styled-components";
-import { useTable, Column } from "react-table";
+import { putSeries } from "../../store/series/actions";
+import { Serie } from "../../store/series/types";
 
-interface dataEntry {
+/*
+ * TODO: fix ignore
+ */
+
+interface Row {
   name: string;
-  isfault: string;
+  isfault: boolean;
 }
 
-interface column {
-  Header: string;
-  accessor: string;
+interface TableState {
+  columns: Array<Column<Row>>;
+  data: Row[];
 }
 
 export default function MaterialTableDemo() {
   const seriesData = useSelector((state: RootState) => state.series.series);
   const dispatch = useDispatch();
-
-  const data: dataEntry[] = useMemo(
-    () => [
-      {
-        name: "Hello",
-        isfault: "World",
-      },
-      {
-        name: "react-table",
-        isfault: "rocks",
-      },
-      {
-        name: "whatever",
-        isfault: "you want",
-      },
+  const [state, setState] = React.useState<TableState>({
+    columns: [
+      { title: "Name", field: "name" },
+      { title: "Isfault", field: "isfault", type: "boolean" },
     ],
-    []
-  );
-
-  const columns: Column<dataEntry>[] = useMemo(
-    () => [
-      {
-        Header: "Name",
-        accessor: "name",
-      },
-      {
-        Header: "isfault",
-        accessor: "isfault",
-      },
-    ],
-    []
-  );
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({ columns, data });
+    data: [],
+  });
 
   return (
-    <table {...getTableProps()} style={{ border: "solid 1px grey" }}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  borderBottom: "solid 3px blue",
-                  background: "aliceblue",
-                  color: "black",
-                  fontWeight: "bold",
-                }}
-              >
-                {column.render("Header")}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    style={{
-                      padding: "10px",
-                      border: "solid 1px gray",
-                    }}
-                  >
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <MaterialTable
+      title="Editable Example"
+      columns={state.columns}
+      data={seriesData}
+      actions={[
+        {
+          icon: "edit",
+          tooltip: "edit",
+          onClick: (event, rowData) => {
+            dispatch(putSeries({ name: "NewSerie", isfault: false }));
+            // @ts-ignore
+            dispatch(putSeries(rowData));
+          },
+        },
+      ]}
+    />
   );
 }
