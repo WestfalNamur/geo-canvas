135710f593f116c691816bf90bed9eb31eb29816
Stefan Crummenerl <crummenerls@gmail.com>
2020-06-15 18:27:27 +0200

    [ENH] bug fixing store

--- src/store/geoData/surfacePoints/reducers.ts
+++ src/store/geoData/surfacePoints/reducers.ts
@@ -1,9 +1,13 @@
 import {
   SurfacePointsState,
   SurfacePointActionTypes,
-  GET_SURFACEPOINTS,
+  ADD_SURFACEPOINTS_FROM_SERVER,
+  ADD_SURFACEPOINT,
+  ADD_SURFACEPOINT_FAILED,
   PUT_SURFACEPOINT,
+  PUT_SURFACEPOINT_FAILED,
   DELETE_SURFACEPOINT,
+  DELETE_SURFACEPOINT_FAILED,
 } from "./types";
 
 const initialState: SurfacePointsState = {
@@ -15,22 +19,49 @@ export function surfacePointsReducer(
   action: SurfacePointActionTypes
 ): SurfacePointsState {
   switch (action.type) {
-    case GET_SURFACEPOINTS:
+    case ADD_SURFACEPOINTS_FROM_SERVER:
       return {
         surfacePoints: action.payload,
       };
+    case ADD_SURFACEPOINT:
+      return {
+        surfacePoints: [...state.surfacePoints, action.payload],
+      };
+    case ADD_SURFACEPOINT_FAILED:
+      return {
+        surfacePoints: state.surfacePoints.filter(
+          (surfacePoint) => surfacePoint.id !== action.payload.id
+        ),
+      };
     case PUT_SURFACEPOINT:
-      const filteredSurfacePoints = state.surfacePoints.filter(
-        (surfacePoint) => surfacePoint.id !== action.payload.id
+      const filteredSurfacePointsPut = state.surfacePoints.filter(
+        (surfacePoint) => surfacePoint.id !== action.payload.oldSurfacePoint.id
       );
       return {
-        surfacePoints: [...filteredSurfacePoints, action.payload],
+        surfacePoints: [
+          ...filteredSurfacePointsPut,
+          action.payload.newSurfacePoint,
+        ],
+      };
+    case PUT_SURFACEPOINT_FAILED:
+      const filteredSurfacePointsPutFailed = state.surfacePoints.filter(
+        (surfacePoint) => surfacePoint.id !== action.payload.newSurfacePoint.id
+      );
+      return {
+        surfacePoints: [
+          ...filteredSurfacePointsPutFailed,
+          action.payload.oldSurfacePoint,
+        ],
       };
     case DELETE_SURFACEPOINT:
       return {
         surfacePoints: state.surfacePoints.filter(
           (surfacePoint) => surfacePoint.id !== action.payload.id
         ),
+      };
+    case DELETE_SURFACEPOINT_FAILED:
+      return {
+        surfacePoints: [...state.surfacePoints, action.payload],
       };
     default:
       return state;
--- src/store/geoData/surfacePoints/types.ts
+++ src/store/geoData/surfacePoints/types.ts
@@ -1,6 +1,11 @@
 export const GET_SURFACEPOINTS = "GET_SURFACEPOINTS";
+export const ADD_SURFACEPOINTS_FROM_SERVER = "ADD_SURFACEPOINTS_FROM_SERVER";
+export const ADD_SURFACEPOINT = "ADD_SURFACEPOINT";
+export const ADD_SURFACEPOINT_FAILED = "ADD_SURFACEPOINT_FAILED";
 export const PUT_SURFACEPOINT = "PUT_SURFACEPOINT";
+export const PUT_SURFACEPOINT_FAILED = "PUT_SURFACEPOINT_FAILED";
 export const DELETE_SURFACEPOINT = "DELETE_SURFACEPOINT";
+export const DELETE_SURFACEPOINT_FAILED = "DELETE_SURFACEPOINT_FAILED";
 
 export interface SurfacePoint {
   id: string;
@@ -20,12 +25,37 @@ export interface SurfacePointsState {
 
 export interface GetSurfacePointsActionType {
   type: typeof GET_SURFACEPOINTS;
+}
+
+export interface AddSurfacePointsFromServerActionType {
+  type: typeof ADD_SURFACEPOINTS_FROM_SERVER;
   payload: SurfacePoint[];
+}
+
+export interface AddSurfacePointActionType {
+  type: typeof ADD_SURFACEPOINT;
+  payload: SurfacePoint;
+}
+
+export interface AddSurfacePointFailedActionType {
+  type: typeof ADD_SURFACEPOINT_FAILED;
+  payload: SurfacePoint;
 }
 
 export interface PutSurfacePointActionType {
   type: typeof PUT_SURFACEPOINT;
-  payload: SurfacePoint;
+  payload: {
+    oldSurfacePoint: SurfacePoint;
+    newSurfacePoint: SurfacePoint;
+  };
+}
+
+export interface PutSurfacePointFailedActionType {
+  type: typeof PUT_SURFACEPOINT_FAILED;
+  payload: {
+    oldSurfacePoint: SurfacePoint;
+    newSurfacePoint: SurfacePoint;
+  };
 }
 
 export interface DeleteSurfacePointActionType {
@@ -33,7 +63,17 @@ export interface DeleteSurfacePointActionType {
   payload: SurfacePoint;
 }
 
+export interface DeleteSurfacePointFailedActionType {
+  type: typeof DELETE_SURFACEPOINT_FAILED;
+  payload: SurfacePoint;
+}
+
 export type SurfacePointActionTypes =
   | GetSurfacePointsActionType
+  | AddSurfacePointsFromServerActionType
+  | AddSurfacePointActionType
+  | AddSurfacePointFailedActionType
   | PutSurfacePointActionType
-  | DeleteSurfacePointActionType;
+  | PutSurfacePointFailedActionType
+  | DeleteSurfacePointActionType
+  | DeleteSurfacePointFailedActionType;
