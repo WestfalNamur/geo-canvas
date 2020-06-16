c9cd140cc476ed5f94a0a91bb6ae0dd81e1ed95a
Stefan Crummenerl <crummenerls@gmail.com>
2020-06-16 10:04:31 +0200

    [ENH] Move load sever data to main app component

--- src/components/geoData/GeoData.tsx
+++ src/components/geoData/GeoData.tsx
@@ -1,25 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ExtentForm from "./ExtentForm";
 import SeriesTable from "./SeriesTable";
 import SurfacesTable from "./SurfacesTable";
 import SurfacePointsTable from "./SurfacePointsTable";
 import OrientationsTable from "./OrientationsTable";
-import { useDispatch } from "react-redux";
-
-import { getSeries } from "../../store/geoData/series/actions";
-import { getSurfaces } from "../../store/geoData/surfaces/actions";
-import { getSurfacePoints } from "../../store/geoData/surfacePoints/actions";
-import { getOrientations } from "../../store/geoData/orientations/actions";
 
 export default function GeoData() {
-  // fetch data from server
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getSeries());
-    dispatch(getSurfaces());
-    dispatch(getSurfacePoints());
-    dispatch(getOrientations());
-  });
   return (
     <div className="GeoData">
       <SeriesTable />
